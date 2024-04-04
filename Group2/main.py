bceee740c26ec3ccda49662b67a34d8c3fe37be3
Jason <jason102344@outlook.com>
2024-04-04 12:52:25 +0100

    YOLO fix
    Fixed error 13 and added the CLI method

--- Group2/main.py
+++ Group2/main.py
@@ -1,8 +1,11 @@
 from ultralytics import YOLO
 
-model = YOLO('yolov8n.pt') # This loads a pretrained model that is recommended for training
+model = YOLO('yolov8n-cls.pt') # This loads a pretrained model that is recommended for training
 
 results = model.train(data='C:/Users/Jason/OneDrive/Desktop/weather_dataset', epochs=1, imgsz=64)
-# setting echochs to 1 for a easy training to see if every works properly
+# setting echochs to 1 for a easy training to see if everything works properly
 # data is the path to the images 
 # Error 13 on my laptop ask zack to see if same 
+
+# CLI method
+# yolo classify train data='C:/Users/Jason/OneDrive/Desktop/weather_dataset' model=yolov8n-cls.pt epochs=1 imgsz=64
